45184069415d4a8e25bedeca3aa273643836fc36
Biswarup35 <biswarup35@gmail.com>
2021-09-25 12:24:39 +0530

    Fix theme import

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
-// import theme from "../src/theme";
-// import createEmotionCache from "../src/createEmotionCache";
+import theme from "../src/theme";
 
 /**
  * Home Page of the Application
@@ -33,7 +32,7 @@ export default class MyDocument extends Document {
       <Html lang="en">
         <Head>
           {/* PWA primary color */}
-          {/* <meta name="theme-color" content={theme.palette.primary.main} /> */}
+          <meta name="theme-color" content={theme.palette.primary.main} />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
