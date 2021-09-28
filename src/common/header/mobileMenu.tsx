c83e50455309b1b66be2d66d7e7e67001d6e2305
Biswarup35 <biswarup35@gmail.com>
2021-09-28 21:18:05 +0530

    Mobile Navigation

---
+++ src/common/header/mobileMenu.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import {
+  List,
+  ListItem,
+  ListItemText,
+  SwipeableDrawer,
+  SwipeableDrawerProps,
+} from "@mui/material";
+import * as React from "react";
+
+interface MobileMenuProps extends SwipeableDrawerProps {}
+
+const MobileMenu: React.FunctionComponent<MobileMenuProps> = ({
+  onClose,
+  onOpen,
+  open,
+}) => {
+  return (
+    <React.Fragment>
+      <SwipeableDrawer open={open} onOpen={onOpen} onClose={onClose}>
+        <List sx={{ minWidth: 240 }}>
+          <Link href="/" passHref>
+            <ListItem button>
+              <ListItemText primary="Home" />
+            </ListItem>
+          </Link>
+          <Link href="/earbuds" passHref>
+            <ListItem button>
+              <ListItemText primary="Earbuds" />
+            </ListItem>
+          </Link>
+          <Link href="/speakers" passHref>
+            <ListItem button>
+              <ListItemText primary="Speakers" />
+            </ListItem>
+          </Link>
+        </List>
+      </SwipeableDrawer>
+    </React.Fragment>
+  );
+};
+
+export default MobileMenu;
